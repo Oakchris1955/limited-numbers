33385969f210432220bfb7ed76d94504bcd5293d
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-05-30 17:36:52 +0300

    Updated development status

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 classifiers = [
-	'Development Status :: 1 - Planning',
+	'Development Status :: 4 - Beta',
 	'Environment :: Console',
 	'Intended Audience :: Developers',
 	'License :: OSI Approved :: MIT License',
